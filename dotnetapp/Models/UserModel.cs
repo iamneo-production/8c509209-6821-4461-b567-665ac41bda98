c634cfde53fd90c993b3ceeded6f1b483bc1eb70
devjeet88 <chowdhurydevjeet99@gmail.com>
2023-04-30 11:23:09 +0000

    final  commit

---
+++ dotnetapp/Models/UserModel.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dotnetapp.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(14)]
+        public string UserName { get; set; }
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string MobileNumber { get; set; }
+        [Required]
+        [MaxLength(14)]
+        public string UserRole { get; set; }
+        [System.Text.Json.Serialization.JsonIgnore]
+        public ICollection<ProductModel> productModels { get; set; }
+       
+
+    }
+}
